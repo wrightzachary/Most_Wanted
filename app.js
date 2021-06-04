a03f71cb5dc66a40a995eebc89c3a3281aea82de
wrightzachary <taylorzw96@gmail.com>
2021-06-04 11:43:53 -0500

    Adding JS files and index HTML

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function searchByName(){
+    // Grabbing the values from our nameForm form and inputs.
+    let firstNameInput = document.forms['nameForm']['fname'].value;
+    let lastNameInput = document.forms['nameForm']['lname'].value;
+
+    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
+    let filteredPeople = people.filter(function (person) {
+        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+            return true;
+        }
+        return false;
+    });
+    
+    // Rather than console logging, you need to append the filteredPeople to a table.
+    if(filteredPeople.length > 0){
+        console.log(filteredPeople);
+    }else{
+        console.log('Sorry, looks like there is no one with that name.');
+    }
+}
