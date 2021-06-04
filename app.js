577e867a135daf385180bb4135396f412766e509
wrightzachary <taylorzw96@gmail.com>
2021-06-04 13:49:51 -0500

    Built table and created CSS file

--- app.js
+++ app.js
@@ -20,3 +20,22 @@ function searchByName(){
         console.log('Sorry, looks like there is no one with that name.');
     }
 }
+function printName(){
+    document.getElementById("people").innerHTML
+}
+printName()
+people.map(function(el){
+    document.getElementById("people").innerHTML += `<tr>
+    <td id=${el.id}>${el.id}</td>
+    <td>${el.firstName}</td>
+    <td>${el.lastName}</td>
+    <td>${el.gender}</td>
+    <td>${el.dob}</td>
+    <td>${el.height}</td>
+    <td>${el.weight}</td>
+    <td>${el.eyeColor}</td>
+    <td>${el.occupation}</td>
+    <td>${el.parents}</td>
+    <td>${el.currentSpouse}</td>
+    </tr>`
+})
