ce59b1bcd8aee8351e756df66e2967e47060c0ef
wrightzachary <taylorzw96@gmail.com>
2021-06-09 22:16:07 -0500

    Added CSS styling, photos to gallery HTML with description, cleaned up some code on app JS

--- app.js
+++ app.js
@@ -289,4 +289,15 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
